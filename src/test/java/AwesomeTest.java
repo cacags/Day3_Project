7719424e5c7ab3f4c9903d629988c0cd2e022709
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-06 21:59:15 +0200

    added more awesome step

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,4 +5,9 @@ public class AwesomeTest {
     //grey in .idea folder means it s ignored
     //when the color is normal, it means it is committed no changes
 
+    //I just pulled the new change from GitHub that I did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now I made more awesome changes and I am about to push this back to GitHub
+
 }
