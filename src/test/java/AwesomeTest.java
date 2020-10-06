2a84a1b557ba87d29b05cd30f3a0e1bb8c1c6e93
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-06 21:46:15 +0200

    Initial Awesome Commit

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,8 @@
+public class AwesomeTest {
+
+    //red means its untracked
+    //green means its under staging area
+    //grey in .idea folder means it s ignored
+    //when the color is normal, it means it is committed no changes
+
+}
